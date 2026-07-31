1fca2959d7a335118a5c06d99372b6e10166004c
GRANDxPAPPY <128447544+GRANDxPAPPY@users.noreply.github.com>
2026-07-31 16:44:43 -0700

    Add: merge-two-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-3.cs
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        while(l1 != null && l2 != null)
+        {
+            if(l1.val <= l2.val)
+            {
+                curr.next = l1;
+                curr = curr.next;
+                l1 = l1.next;
+            }
+            else
+            {
+                curr.next = l2;
+                curr = curr.next;
+                l2 = l2.next;
+            }
+
+        }
+        if(l1 != null)
+        {
+            curr.next = l1;
+            
+        }
+        if(l2 != null)
+        {
+            curr.next = l2;
+            
+        }
+        return dummyHead.next;
+    }
+}
